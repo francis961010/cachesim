fffdd61dd0dd39a159198b57d68ec5e0506492b4
francis961010 <31148685+francis961010@users.noreply.github.com>
2021-07-28 00:06:04 -0300

    Add files via upload
    CacheSim finalizado :)

--- main.c
+++ main.c
@@ -7,6 +7,16 @@
 #define ERROR -1
 #define MISS_PENALTY 100
 
+typedef struct verboso{
+    bool validez_previo;
+    bool birtybit_previo;
+    int operacion_anterior;
+    int numero_linea;
+    int tag_previo;
+    int caso;
+} Verboso;
+
+
 typedef struct linea {
     bool validez;
     bool dirtybit;
@@ -75,12 +85,36 @@ void seti_y_tag(u_int32_t *direccion, int tamanio_seti, int tamanio_block_offset
     *tag = mask_tdos & aux;
 }
 
+char *deci_2_hexa(int decimal){
+    char hexa_reverso[20];
+    int i = 0;
+    while(decimal != 0){
+        int aux = decimal%16;
+        if(aux < 10){
+            hexa_reverso[i] = aux + 48;
+            i++;
+        }
+        else{
+            hexa_reverso[i] = aux + 55;
+            i++;
+        }
+        decimal = decimal/16;
+    }
+    char hexadecimal[i-1];
+    int k = 0;
+    for(int j = i-1; j >= 0; j--){
+        hexadecimal[k] = hexa_reverso[j];
+        k++;
+    }
+    char *hexa = hexadecimal;
+    return hexa;
+}
+
 void crear_lineas(Set *set, int cant_lineas){ 
 
     set->lineas = calloc(cant_lineas, sizeof(Linea*));
-    if(!set->lineas){
+    if(!set->lineas)
         return;
-    }
     for(int i = 0; i < cant_lineas; i++){
         set->lineas[i] = calloc(1, sizeof(Linea));
         set->lineas[i]->validez = false;
@@ -230,21 +264,98 @@ void modo_simple(Cache *cache, int tag, int set_index, Entrada *entrada, int byt
     if(set_actual->cantidad_lineas == set_actual->lineas_ocupadas)
         set_esta_lleno = true;
 
-    if(!linea && !set_esta_lleno){
+    if(!linea && !set_esta_lleno)
         linea_fria(set_actual, cache->metrics, tag, entrada, bytesxbloque);
-    }
+
     else if(!linea && set_esta_lleno){
         Linea *ultima_linea_usada = least_recently_used(set_actual);
-        if(ultima_linea_usada->dirtybit == false){
+        if(ultima_linea_usada->dirtybit == false)
             linea_clean_miss(set_actual, cache->metrics, tag, entrada, ultima_linea_usada, bytesxbloque);
+        else
+            linea_dirty_miss(set_actual, cache->metrics, tag, entrada, ultima_linea_usada, bytesxbloque);
+    }
+    else if(linea && linea->validez == true)
+        linea_hit(linea, cache->metrics, tag, entrada);
+}
+
+void encontrar_caso(Set *set_actual, Linea *linea, int encontro_linea, Verboso *datos){
+
+    bool set_esta_lleno = false;
+    if(set_actual->cantidad_lineas == set_actual->lineas_ocupadas)
+        set_esta_lleno = true;
+
+    if(encontro_linea == 1 && !set_esta_lleno){
+        datos->caso = 1;
+    }
+    else if(encontro_linea == 1 && set_esta_lleno){
+        if(linea->dirtybit == false){
+           datos->caso = 1;
         }
         else{
-            linea_dirty_miss(set_actual, cache->metrics, tag, entrada, ultima_linea_usada, bytesxbloque);
+            datos->caso = 2;
         }
     }
-    else if(linea && linea->validez == true){
-        linea_hit(linea, cache->metrics, tag, entrada);
+    else if(encontro_linea == 0 && linea->validez == true){
+        datos->caso = 0;
     }
+}
+
+void linea_datos_verboso(Set *set_actual, int tag, Verboso *datos){
+    Linea *linea = encontrar_linea(set_actual, tag);
+    int es_valido = 0;
+    if(!linea){
+        es_valido = 1;
+        linea = least_recently_used(set_actual);
+    }
+    encontrar_caso(set_actual, linea, es_valido, datos);
+    datos->birtybit_previo = linea->dirtybit;
+    if(!linea->tag)
+        datos->tag_previo = -1;
+    else
+        datos->tag_previo = linea->tag; 
+    datos->validez_previo = linea->validez;
+    datos->operacion_anterior = linea->ultima_operacion;
+    for(int i = 0; i < set_actual->cantidad_lineas; i++){
+        if(linea->tag == set_actual->lineas[i]->tag)
+            datos->numero_linea = i;
+    }
+}
+
+void modo_verboso(Cache *cache, int tag, int set_index, Entrada *entrada){
+
+    Set *set_actual = cache->sets[set_index];
+    Verboso *datos_verboso = calloc(1, sizeof(Verboso));
+    linea_datos_verboso(set_actual, tag, datos_verboso);
+    printf("%i ", entrada->numero_de_operacion);
+    switch (datos_verboso->caso){                           
+        case 0:                                           
+            printf("1 ");                                
+            break;                                        
+        case 1:                                          
+            printf("2a ");                              
+            break;                                      
+        case 2:                                          
+            printf("2b ");                              
+            break;                                        
+    }                                                   
+    char hex_1[4];
+    sprintf(hex_1, "%x", set_index);
+    printf("%s ", hex_1);     
+    char hex_2[10];
+    sprintf(hex_2, "%x", tag);
+    printf("%s ", hex_2);      
+    printf("%i ", datos_verboso->numero_linea);     
+    if(datos_verboso->tag_previo == -1)                       
+        printf("%d ", datos_verboso->tag_previo);            
+    else{                                                        
+        char hex_3[10];
+        sprintf(hex_3, "%x", datos_verboso->tag_previo);
+        printf("%s ", hex_3);  
+    }
+    printf("%d ", datos_verboso->validez_previo);   
+    printf("%d ", datos_verboso->birtybit_previo);  
+    printf("%d\n", datos_verboso->operacion_anterior);  
+    free(datos_verboso);
 }
 
 void devolver_resumen(Cache *cache, int E, int tamanio_cache){
@@ -265,9 +376,8 @@ void destruir_cache(Cache *cache){
     
     for(int i = 0; i < cache->cantidad_sets; i++){
         Set *set_actual = cache->sets[i];
-        for(int j = 0; j < set_actual->cantidad_lineas; j++){
+        for(int j = 0; j < set_actual->cantidad_lineas; j++)
             free(set_actual->lineas[j]);
-        }
         free(set_actual->lineas);
         free(set_actual);
     }
@@ -300,19 +410,21 @@ int main(int argc, char const *argv[]){//argv[4] -> nro de Sets; argv[3] -> asoc
 
         Entrada *entrada = calloc(1, sizeof(Entrada));
         fscanf(archivo, "%i: %c %i %i %i", &(entrada->instruction_pointer), &(entrada->operacion), &(entrada->direccion), &(entrada->tamanio_operacion), &(entrada->datos_leidos)); // guardo datosa de la liena;
-        
+
         entrada->numero_de_operacion = numero_de_operacion;
-        if(entrada->instruction_pointer == 0 && entrada->operacion == 0 && entrada->direccion == 0 && entrada->tamanio_operacion == 0 && entrada->datos_leidos == 0){
+        if(entrada->instruction_pointer == 0 && entrada->operacion == 0 && entrada->direccion == 0 && entrada->tamanio_operacion == 0 && entrada->datos_leidos == 0)
             break;
-        }
+    
         int set_index;
         int tag;
         int bloque;
         int bytesxbloque = (atoi(argv[2]) / (atoi(argv[3]) * atoi(argv[4])));
-       
         seti_y_tag(&(entrada->direccion), log2(atoi(argv[4])), log2(bytesxbloque) ,&set_index, &tag);
-        if(!es_verboso)
-            modo_simple(cache, tag, set_index, entrada, bytesxbloque);
+        
+        if(es_verboso && entrada->numero_de_operacion >= atoi(argv[6]) && entrada->numero_de_operacion <= atoi(argv[7]))
+            modo_verboso(cache, tag, set_index, entrada);
+        modo_simple(cache, tag, set_index, entrada, bytesxbloque);
+        
         free(entrada);
         numero_de_operacion++;
         
